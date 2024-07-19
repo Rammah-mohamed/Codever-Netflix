52e1060daacc73a5a4cff28381479075ace3a226
Rammah <dev.rammah@gmail.com>
2024-07-19 23:08:36 +0300

    Reset API

--- src/utils/fetchFromApi.js
+++ src/utils/fetchFromApi.js
@@ -2,14 +2,14 @@ import axios from "axios";
 const BASE_URL = "https://movies-api14.p.rapidapi.com";
 
 const options = {
-	method: "GET",
-	headers: {
-		"X-RapidAPI-Key": "bfaee7d883mshb2114ef5350b018p1978ddjsneef119ed3fe5",
-		"X-RapidAPI-Host": "movies-api14.p.rapidapi.com",
-	},
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "bff455d4dfmsh196bf9b08bacb33p1efd8bjsn485fc9dac3a6",
+    "X-RapidAPI-Host": "movies-api14.p.rapidapi.com",
+  },
 };
 
 export const fetchFromApi = async (query) => {
-	const { data } = await axios.get(`${BASE_URL}/${query}`, options);
-	return data;
+  const { data } = await axios.get(`${BASE_URL}/${query}`, options);
+  return data;
 };
